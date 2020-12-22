df8cf12fda350cb0089fe27e88cb1d11769d955a
vaibhav gupta <vaigupta@egencia.com>
2020-12-22 17:39:10 +0530

    image

--- Classificaition.py
+++ Classificaition.py
@@ -18,7 +18,7 @@ import streamlit.components.v1 as components
 
 
 
-st.set_page_config(page_title="DataZilla",page_icon='/Users/vaigupta/Documents/DS/StreamLit/zilla.jpeg',  initial_sidebar_state = 'auto')
+st.set_page_config(page_title="DataZilla",page_icon='zilla.jpeg',  initial_sidebar_state = 'auto')
 st.image('/Users/vaigupta/Documents/DS/StreamLit/zilla.jpeg',width=500,use_column_width=True)
 
 
